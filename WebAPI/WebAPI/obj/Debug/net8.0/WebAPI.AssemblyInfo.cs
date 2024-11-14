f710089a650c54855ad35f05a690a3986396f4e5
Thanh Pham <phamhoangthanh1582003@gmail.com>
2024-11-14 09:42:20 +0700

    Add-Product-Brand

--- WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13612bbdee3f5ce14314bcbe9fb5712fef45e053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a07777aa7a0bf682be7e0ad032ca0090474616")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
