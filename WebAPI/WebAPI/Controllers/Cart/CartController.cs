44f62f0b0d5f679070df38260e4ec40743cdd3b0
thangnvhe <thangnvhe171327@fpt.edu.vn>
2024-11-17 12:18:04 +0700

    Cart Function

---
+++ WebAPI/WebAPI/Controllers/Cart/CartController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Repository;
+
+namespace WebAPI.Controllers.Cart
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartRepository;
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+        [HttpGet("get-cart")]
+        public async Task<IActionResult> GetCartsOfUser(string userEmail)
+        {
+            var cartItems = _cartRepository.GetCartItemsByUserIdAsync(userEmail);
+            return Ok(await cartItems);
+        }
+
+        [HttpPost("add-cart-item")]
+        public async Task<IActionResult> AddCartItem(string userEmail, Guid productd, int quantity)
+        {
+            var result = await _cartRepository.AddCartItemAsync(userEmail, productd, quantity);
+            if (!result)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to add cart item");
+            }
+            return Ok(result);
+        }
+
+        [HttpPut("update-quantity")]
+        public async Task<IActionResult> UpdateQuantityCartItem(Guid cartId, int quantity)
+        {
+            var result = await _cartRepository.UpdateQuantityCartItemAsync(cartId, quantity);
+            if (!result)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to update quantity");
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("delete-cart-item")]
+        public async Task<IActionResult> DeleteCartItem(Guid cartItemId)
+        {
+            var result = await _cartRepository.DeleteCartItemAsync(cartItemId);
+            if (!result)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Failed to delete cart item");
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Repository/IRepository/ICartRepository.cs
@@ -0,0 +1,13 @@
+﻿using WebAPI.ViewModel;
+
+namespace WebAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<IEnumerable<CartIViewModel>> GetCartItemsByUserIdAsync(string email);
+        Task<CartIViewModel> GetCartItemAsync(Guid cartItemId);
+        Task<bool> AddCartItemAsync(string userEmail, Guid productd, int quantity);
+        Task<bool> UpdateQuantityCartItemAsync(Guid cartId, int quantity);
+        Task<bool> DeleteCartItemAsync(Guid cartItemId);
+    }
+}
